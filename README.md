96fa9160ec1d2b70a27d036108aa1f070fd2fa19
Karl Krukow <kakrukow@microsoft.com>
2018-03-21 17:21:53 -0700

    Initial template project

---
+++ README.md
@@ -0,0 +1,7 @@
+# vsts-flow-metrics
+Tool for visualizing flow metrics from Visual Studio Team Services data## License
+
+# License
+Copyright © 2018 Karl Krukow
+
+Distributed under the MIT License.
---
+++ test/vsts_flow_metrics/core_test.clj
@@ -0,0 +1,7 @@
+(ns vsts-flow-metrics.core-test
+  (:require [clojure.test :refer :all]
+            [vsts-flow-metrics.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
