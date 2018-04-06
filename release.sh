d927d69ea87d929bfd22890340d95e9138dc5811
Karl Krukow <kakrukow@microsoft.com>
2018-04-06 13:38:59 +0200

    Run smoke test in release.sh

--- release.sh
+++ release.sh
@@ -4,7 +4,13 @@ cd $(dirname $0)
 PROJECT_DIR=$(pwd)
 PROJECT_NAME=$(basename $PROJECT_DIR)
 
+echo "Building..."
+
 ./build.sh
+
+echo "Running smoke tests..."
+
+./smoke-test.sh
 
 echo "Releasing project '$PROJECT_NAME' in $PROJECT_DIR version: $1"
 
