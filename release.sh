8d9d509c2446dec34ce4b2d547f739d93c231432
Karl Krukow <kakrukow@microsoft.com>
2018-04-18 21:52:31 +0200

    Bump to 1.0.5 for release

--- release.sh
+++ release.sh
@@ -15,6 +15,6 @@ echo "Running smoke tests..."
 echo "Releasing project '$PROJECT_NAME' in $PROJECT_DIR version: $1"
 
 rm -f flow-metrics-$1.zip
-zip -r -X flow-metrics-$1.zip flow-metrics target/flow-metrics.jar wiql doc
+zip -r -X flow-metrics-$1.zip flow-metrics target/flow-metrics.jar wiql doc project.clj
 
 echo "Created flow-metrics-$1.zip"
