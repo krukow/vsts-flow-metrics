4368aa9d77ec0eb7b4fb4cac4fc0fd7c5a8bd178
Karl Krukow <kakrukow@microsoft.com>
2018-05-17 15:51:45 +0200

    Make smoke tests more accessible

--- smoke-test.sh
+++ smoke-test.sh
@@ -2,25 +2,24 @@
 set -x
 set -e
 
-echo "Warning this requires private data: cache/2018-04-06T07:02-closed-stories-15d.wiql.json"
-echo "Warning this requires private data: cache/2018-04-06T07:11-closed-features-30d.wiql.json"
+echo "Warning this requires private data: cache/sample.json"
 
 echo "If you don't have this please generate your own data for smoke tests"
 echo "Running smoke tests"
 
 ./flow-metrics show-config
 
-./flow-metrics flow-efficiency cache/2018-04-06T07:02-closed-stories-15d.wiql.json
+./flow-metrics flow-efficiency cache/sample.json
 
-./flow-metrics time-in-state cache/2018-04-06T07:11-closed-features-30d.wiql.json
+./flow-metrics time-in-state cache/sample.json
 
-./flow-metrics cycle-time cache/2018-04-06T07:11-closed-features-30d.wiql.json --chart features-closed-30d-2018-04-06.svg
+./flow-metrics cycle-time cache/sample.json --chart sample.svg
 
-./flow-metrics cycle-time cache/2018-04-06T07:11-closed-features-30d.wiql.json
+./flow-metrics cycle-time cache/sample.json
 
-./flow-metrics responsiveness cache/2018-04-06T07:11-closed-features-30d.wiql.json
+./flow-metrics responsiveness cache/sample.json
 
-./flow-metrics lead-time-distribution cache/2018-04-06T07:02-closed-stories-15d.wiql.json
+./flow-metrics lead-time-distribution cache/sample.json
 
 echo "Warning slower commands (uses API)"
 ./flow-metrics historic-queues wiql/features-as-of-template.wiql
