548e1331fc1dbd0d76cd83bc412be8d7fad76e95
Karl Krukow <kakrukow@microsoft.com>
2018-08-10 14:54:27 -0700

    Bump to snapshot release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vsts-flow-metrics "1.0.9"
+(defproject vsts-flow-metrics "1.0.10-SNAPSHOT"
   :description "Tool for visualizing flow metrics from Visual Studio Team Services data"
   :url "https://github.com/krukow/vsts-flow-metrics"
   :license {:name "MIT"
